3c01ce48709327928b9b623b5b4307436069c807
augustomontanha-netizen <augusto.montanha@escola.pr.gov.br>
2026-07-28 12:07:13 -0300

    Update questions and answers in perguntas.js

--- perguntas.js
+++ perguntas.js
@@ -12,8 +12,8 @@ criaCartao(
 
 criaCartao(
     'Programação',
-    'O que é uma função?',
-    'Uma função é um bloco de código que executa alguma tarefa'
+    'O que é HTML?',
+    'HTML é a linguagem de marcação padrão usada para criar e estruturar páginas na web'
 )
 
 criaCartao(
